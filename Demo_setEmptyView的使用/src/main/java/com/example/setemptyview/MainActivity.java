c807d74cba16b93d6b0bb1ad98e0acef815d619f
539863010 <https://github.com/539863010/uubc_hz.git>
2016-05-20 14:28:05 +0800

    测测测测~~~~~~~~~~~~

--- Demo_setEmptyView的使用/src/main/java/com/example/setemptyview/MainActivity.java
+++ Demo_setEmptyView的使用/src/main/java/com/example/setemptyview/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         list1.add("xxx");
         list1.add("xxx");
 
-
+        //ssssssssssssssssss
 //        View emptyView = findViewById(R.id.button);
 
         ListView listView = (ListView) findViewById(R.id.listView);
